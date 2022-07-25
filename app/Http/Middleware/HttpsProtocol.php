a97440715595c213106b06b44e79da98a76c4b0a
Nathan Perfetti <103277729+np-compozelabs@users.noreply.github.com>
2022-07-24 20:25:19 -0500

    configured https for redirects with middleware

--- app/Http/Middleware/HttpsProtocol.php
+++ app/Http/Middleware/HttpsProtocol.php
@@ -1,23 +1,22 @@
 <?php
 
 namespace App\Http\Middleware;
- 
+
 use Closure;
+use Illuminate\Http\Request;
 
 class HttpsProtocol
 {
     /**
      * Handle an incoming request.
      *
-     * @param    \Illuminate\Http\Request  $request
-     * @param    \Closure  $next
-     * @return  mixed
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
-    public function handle($request, Closure $next)
+    public function handle(Request $request, Closure $next)
     {
-        if (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) &&  $_SERVER['HTTP_X_FORWARDED_PROTO'] == 'http'
-         && \App::environment() === 'production') {
-            return redirect()->secure($request->getRequestUri());
-        }
+        return $next($request);
     }
 }
+
